1c76eeedb9beb710887054be399fc3b5cbec775b
Manoa Razafimahandry <jrmanouhoseah@gmail.com>
2025-10-21 15:43:32 +0300

    first commit

---
+++ src/main/java/com/devix/demo/DeviXApplication.java
@@ -0,0 +1,13 @@
+package com.devix.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeviXApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeviXApplication.class, args);
+	}
+
+}
